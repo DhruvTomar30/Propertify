042eab32f8be1cf848bcd9892c9515764ad09fdd
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-06-06 15:32:15 +0530

    user model

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test=(req,resp) =>{
+    resp.json({
+        message:"Api is Working"
+    })
+}
--- api/index.js
+++ api/index.js
@@ -1,14 +1,31 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
 dotenv.config();
+const app=express();
+// app.use(express.json());
+
+
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to MongoDB');
 }).catch((err)=>{
     console.log(err);
 })
-const app=express();
+
+app.use('/api/user',userRouter);
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
